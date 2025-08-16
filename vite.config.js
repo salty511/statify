5faeacd75cacad15f359abae22a336a63eb4f2b3
Isaac <isaac.w.salt@gmail.com>
2025-08-16 01:03:41 +0100

    Fixed dotenv for login url

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env.VITE_SPOTIFY_CLIENT_ID': JSON.stringify(process.env.VITE_SPOTIFY_CLIENT_ID),
-    'process.env.VITE_SPOTIFY_REDIRECT_URI': JSON.stringify(process.env.VITE_SPOTIFY_REDIRECT_URI)
+    'process.env.VITE_LOGIN_URL': JSON.stringify(process.env.VITE_LOGIN_URL),
   },
   server: {
     port: 3000,
